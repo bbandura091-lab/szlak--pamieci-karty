d7145727699365fcb33ef3519337c0bdf3caf23f
bbandura091-lab <bbandura091@gmail.com>
2026-07-31 14:25:02 +0200

    Refactor geoJSON file paths and remove loading code
    Updated file paths for geoJSON files and removed loading logic.

--- app .js
+++ app .js
@@ -11,62 +11,17 @@ const map = new mapboxgl.Map({
 
 // 🔥 Lista Twoich plików geoJSON
 const files = [
-  'pos_001.geojson',
-  'pos_002.geojson',
-  'pos_003.geojson',
-  'pos_004.geojson',
-  'pos_005.geojson',
-  'pos_006.geojson',
-  'pos_007.geojson',
-  'pos_008.geojson',
-  'pos_009.geojson',
-  'pos_010.geojson',
-  'pos_011.geojson',
-  'pos_013.geojson',
-  'pos_014.geojson',
-  'pos_015.geojson',
-  'pos_016.geojson',
-  'pos_017.geojson',
-  'pos_018.geojson',
-  'pos_019.geojson'
+  'data/pos_001.geojson',
+  'data/pos_002.geojson',
+  'data/pos_003.geojson',
+  'data/pos_004.geojson',
+  'data/pos_005.geojson',
+  'data/pos_006.geojson',
+  'data/pos_007.geojson',
+  'data/pos_008.geojson',
+  'data/pos_009.geojson',
+  'data/pos_010.geojson',
+  'data/pos_011.geojson',
+  'data/pos_013.geojson',
+  'data/pos_014.geojson'
 ];
-
-// 🔥 Wczytywanie każdego pliku geoJSON
-files.forEach(file => {
-  fetch(file)
-    .then(res => res.json())
-    .then(data => {
-
-      // Dodaj źródło
-      map.addSource(file, {
-        type: 'geojson',
-        data: data
-      });
-
-      // Dodaj warstwę (znacznik)
-      map.addLayer({
-        id: file,
-        type: 'circle',
-        source: file,
-        paint: {
-          'circle-radius': 8,
-          'circle-color': '#b30000'
-        }
-      });
-
-      // Popup po kliknięciu
-      map.on('click', file, (e) => {
-        const props = e.features[0].properties;
-
-        new mapboxgl.Popup()
-          .setLngLat(e.lngLat)
-          .setHTML(`
-            <h3>${props.name}</h3>
-            <p>${props.role}</p>
-            <a href="${props.page}" target="_blank">Zobacz kartę</a>
-          `)
-          .addTo(map);
-      });
-    })
-    .catch(err => console.error('Błąd wczytywania:', file, err));
-});
